67af257aaaf8b86dc0a3a04d8ebc2c59988be94d
edwardchen <499485532@qq.com>
2021-04-11 23:56:25 +0800

    Example of 23.6 finalizer improved

--- GarbageCollection/finalizer.lua
+++ GarbageCollection/finalizer.lua
@@ -1,13 +1,22 @@
 -- 23.6 析构器
 
-local object = { last_words = "bye" }
-
-setmetatable(object, {__gc = function (o)
-    if o.last_words then
-        print(o.last_words)
+local finalizer = function (object)
+    if object.last_words then
+        print(string.format("\"%s\" from %s", object.last_words, object.name))
     end
-end})
+end
+local mt = {__gc = finalizer}
 
-object = nil
-collectgarbage()
+local ed = { last_words = "bye", name = "edwardchen"}
+local tom = { last_words = "wtf?", name = "tom"}
+
+setmetatable(ed, mt) -- 为ed进行标记
+mt.__gc = nil
+setmetatable(tom, mt) -- tom未被标记
+
+mt.__gc = finalizer -- 设置元表后重新赋值也没用
+
+ed = nil
+tom = nil
+collectgarbage() -- 仅执行ed的析构器
 print("collectgarbage() finished")
